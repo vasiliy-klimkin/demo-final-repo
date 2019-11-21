585665f3b6823521098fa6fe10f883713cf5fb86
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2019-11-21 11:30:17 -0500

    Added button 2

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <button>Second Button</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
