8117a728df98226f36baf38ca2023ace48e762fe
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2019-11-21 11:31:36 -0500

    Added stuff

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <button>asdasda</button>
+      aasdja akuj hksdf kf hdsuflkfhslkfdlks fh
+      dsfkusdfh lsdfhlskd lkfghlks lk;zhsdlka
+      afnljsdflkjsfljfhkjsdlksd fhs
+      sdlfhsdli hslkdfhlk fhslf hlk
+      <button>asdasda</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
